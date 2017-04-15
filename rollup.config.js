6a9e472959e9a0898d9d3c2fbab94b3a7fe921df
Francesco Gramano <fran.gramano@mail.utoronto.ca>
2017-04-15 10:57:30 -0400

    Initiates attempt at transforming starter to template

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,12 @@
+<!-- @if USE_BABEL -->
 import babel from 'rollup-plugin-babel';
 import babelrc from 'babelrc-rollup';
+<!-- @endif -->
+
+<!-- @if CODE_COVERAGE -->
 import istanbul from 'rollup-plugin-istanbul';
+<!-- @endif -->
+
 
 let pkg = require('./package.json');
 let external = Object.keys(pkg.dependencies);
@@ -8,10 +14,15 @@ let external = Object.keys(pkg.dependencies);
 export default {
   entry: 'lib/index.js',
   plugins: [
-    babel(babelrc()),
-    istanbul({
-      exclude: ['test/**/*', 'node_modules/**/*']
-    })
+    <!-- @if USE_BABEL -->
+      babel(babelrc()),
+    <!-- @endif -->
+
+    <!-- @if CODE_COVERAGE -->
+      istanbul({
+          exclude: ['test/**/*', 'node_modules/**/*']
+      })
+    <!-- @endif -->
   ],
   external: external,
   targets: [
